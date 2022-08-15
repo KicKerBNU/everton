a21ccf1d6b2981471a59c280067f399a3a14d848
Everton Alcides Buzzi <evertonbuzzi@gmail.com>
2022-08-15 14:31:48 -0300

    EB: Firebase

--- src/main.js
+++ src/main.js
@@ -20,3 +20,24 @@ const app = createApp(App);
 app.component('Nav', Nav);
 const mountedApp = app.use(router).mount('#app')
 
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyDd4Cw7z2oWywl9XkIh8sBYAtHia_7NsaE",
+    authDomain: "everton-17da5.firebaseapp.com",
+    projectId: "everton-17da5",
+    storageBucket: "everton-17da5.appspot.com",
+    messagingSenderId: "932994133020",
+    appId: "1:932994133020:web:c87ebaf447d3548ba3c3d9",
+    measurementId: "G-KFKCBCH52Z"
+  });
+const auth = getAuth(firebaseApp);
+
+onAuthStateChanged(auth, user => {
+    if(user != null) {
+        console.log("logged in");
+    }else {
+        console.log("No User");
+    }
+})
