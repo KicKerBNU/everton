272a01b20f9d14f5d4ac291b62699d116271cdc0
Everton Buzzi <evertonbuzzi@gmail.com>
2024-03-23 13:27:35 +0000

    Prevent the user to go to a url that dont exist.

--- src/router/router.js
+++ src/router/router.js
@@ -5,14 +5,26 @@ import Documentation from '../views/Documentation.vue';
 import Login from '../views/Login.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-export default createRouter({
+const routes = [
+  { path: '/', name: '/', component: Home },
+  { path: '/home', name: 'Home', component: Home },
+  { path: '/contact', name: 'Contact', component: Contact },
+  { path: '/maps', name: 'Maps', component: Maps },
+  { path: '/doc', name: 'Documentation', component: Documentation },
+  { path: '/login', name: 'Login', component: Login }
+];
+
+const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: '/', name: '/', component: Home },
-    { path: '/home', name: 'Home', component: Home },
-    { path: '/contact', name: 'Contact', component: Contact },
-    { path: '/maps', name: 'Maps', component: Maps },
-    { path: '/doc', name: 'Documentation', component: Documentation },
-    { path: '/login', name: 'Login', component: Login }
-  ]
+  routes
 });
+
+router.beforeEach((to, from, next) => {
+  if (!routes.find(route => route.path === to.path)) {
+    next({ name: '/' });
+  } else {
+    next();
+  }
+});
+
+export default router;
